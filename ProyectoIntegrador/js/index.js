67a51acab24f5ff0d6f06a4ac6077bfd3dafca46
T0M4SCG <tconchagiovana@udesa.edu.ar>
2022-06-09 23:26:41 -0300

    descifre como hacer esta cosa

--- ProyectoIntegrador/js/index.js
+++ ProyectoIntegrador/js/index.js
@@ -1,23 +1,26 @@
-alert("hola")
-
 let url = 'https://api.allorigins.win/raw?url=https://api.deezer.com/chart';
 
-let contenedor = '';
+let contenedor = document.querySelector("#tutio");
+
+
 
 fetch(url)
     .then(function(response){
-    return response.json()
+    return response.json();
     })
-    .then(function(data){
-        console.log(data.results);
-        let canciones = data.results;
-        contenedor = document.querySelector('.container');
-        for (let i = 0; i < canciones.lenght; i++){
-        contenedor.innerHTML += `<article>
-        <img src = ${canciones[i].link} alt=''/>
-         <p>Name:  ${canciones[i].title} </p>
-         <p>Status: ${canciones[i].name} </p>
-</article>`
+    .then(function(datos){
+        console.log(datos)
+        let canciones = datos.albums.data;
+
+        for (let i = 0; i < 5; i++){
+        contenedor.innerHTML += `
+        <article class="articlecanc">
+        <img src="${canciones[i].cover}" alt="" class="fotodiscos">
+        <h4>${canciones[i].title}</h4>
+        <p>${canciones[i].artist.name}</p>
+        <a href="./detalle-album.html" class="verMas">Ver más</a>
+    </article>
+    `
         }
     })
     .catch(function(error){
