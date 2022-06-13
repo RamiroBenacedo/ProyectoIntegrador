f1323290e64e490605e6ea978c5168c0b461ac54
ezebilinkis <ebilinkis@udesa.edu.argit clone https://github.com/RamiroBenacedo/ProyectoIntegrador.git>
2022-06-13 19:54:56 -0300

    generos terminado

---
+++ ProyectoIntegrador/js/generos.js
@@ -0,0 +1,23 @@
+let url = 'https://api.allorigins.win/raw?url=https://api.deezer.com/genre';
+
+for (let i = 1; i < 11; i++){
+fetch(url)
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(datos){
+        console.log(datos)
+        let generos = datos.data[i];
+        let contenedor = document.querySelector('#tutio');
+        
+        contenedor.innerHTML += 
+        `<article class="articlecanc">\
+        <img src="${generos.picture_medium}" alt="" class="fotodiscos">
+        <h4>${generos.name}</h4>
+        <a href="./detalle-genero.html" class="verMas">Ver más</a>
+        </article>`
+        
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    })}
