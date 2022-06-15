6c609c8a033de45ecb0b9c340984bd79c940f823
T0M4SCG <tconchagiovana@udesa.edu.ar>
2022-06-15 12:28:33 -0300

    Termine resultado de busqueda

---
+++ ProyectoIntegrador/js/resultadosDeBusqueda.js
@@ -0,0 +1,32 @@
+let Resultado = document.querySelector(".queso")
+
+let cosasguardada = location.search
+
+let objliteral = new URLSearchParams(cosasguardada)
+
+let resultados = objliteral.get("cancion")
+console.log(resultados);
+let url77 = `https://api.allorigins.win/raw?url=https://api.deezer.com/search?q=${resultados}`
+
+fetch(url77)
+
+.then(function(response){
+    return response.json();
+})
+.then(function(datos) {
+    let imp = datos.data
+
+    console.log(imp);
+    if (imp.length > 0) { 
+        Resultado.innerHTML = `<h1 id="duki">Resultados de Busqueda para: ${resultados}</h1>` 
+        for (let i = 0; i <= imp.length; i++) {
+
+            Resultado.innerHTML += `<a href="./detalle-cancion.html?id=${imp[i].id}">${imp[i].title}</a>`
+          }
+    }
+    else{  Resultado.innerHTML += `<h1 id="duki">No Encontramos Resultados de Busqueda para: ${resultados}</h1>`     
+
+}})
+.catch(function(error){
+    console.log("Error: " + error);
+})
