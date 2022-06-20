ce341750019cdbd0cc08c73a3aa1dce238b28162
RamiroBenacedo <Rbenacedo@udesa.edu.ar>
2022-06-20 16:38:34 -0300

    detalles de genero

--- ProyectoIntegrador/js/detalle-genero.js
+++ ProyectoIntegrador/js/detalle-genero.js
@@ -19,3 +19,22 @@ fetch(urluno)
         </article>`})
     }else {
 }
+
+if(id !== null){
+let urlo = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`;
+
+fetch(urlo)
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(artistas){
+        console.log(artistas)
+        let contenedor4 = document.querySelector("#tutiotres");
+        for (let i = 0; i < 5 ; i++) {
+            contenedor4.innerHTML += `
+        <article class="articlecanc">
+        <img src="${artistas.data[i].picture_medium}" alt="" class="fotodiscos">
+        <h4>${artistas.data[i].name}</h4>
+    </article>`}})
+    }else {
+}
